c698cb5c3f40ea71b52ed25a99212d28e0e8f0ad
miguelyoos <miguelosoriohijo@gmail.com>
2026-06-11 19:14:55 -0500

    cors

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,12 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
+const cors = require('cors');
+app.use(cors({
+  origin: 'http://localhost:5173', // El puerto de tu frontend
+  credentials: true
+}));
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
